f014281535271e0e40243fea5b6f7836e451f00b
Yat Choi <yat.choi@airbnb.com>
2015-07-04 11:00:05 -0700

    add gemfile

--- config.ru
+++ config.ru
@@ -1,3 +1,3 @@
 require './main'
 
-run MainApp
+run Sinatra::Application
--- main.rb
+++ main.rb
@@ -3,34 +3,32 @@ require 'json'
 require "./lib/meme_generator"
 require "./lib/directory_cleaner"
 
-class MainApp < Sinatra::Base
-  get '/' do
-    'Hello World!'
-  end
+get '/' do
+  'Hello World!'
+end
 
-  get '/memegen' do
-    image = params['img'] ||
-      "https://pbs.twimg.com/profile_images/378800000822867536/3f5a00acf72df93528b6bb7cd0a4fd0c.jpeg"
-    header = params['header'] || "Yay it works"
-    footer = params['footer'] || "by default"
+get '/memegen' do
+  image = params['img'] ||
+    "https://pbs.twimg.com/profile_images/378800000822867536/3f5a00acf72df93528b6bb7cd0a4fd0c.jpeg"
+  header = params['header'] || "Yay it works"
+  footer = params['footer'] || "by default"
 
-    DirectoryCleaner::clean
-    img = MemeGenerator::generate(image, header, footer)
+  DirectoryCleaner::clean
+  img = MemeGenerator::generate(image, header, footer)
 
-    return "<img src='/meme/#{img}'/>"
-  end
+  return "<img src='/meme/#{img}'/>"
+end
 
-  post '/memegen' do
-    json_params = JSON.parse(request.body.read)
+post '/memegen' do
+  json_params = JSON.parse(request.body.read)
 
-    image = json_params['img'] ||
-      "https://pbs.twimg.com/profile_images/378800000822867536/3f5a00acf72df93528b6bb7cd0a4fd0c.jpeg"
-    header = json_params['header'] || "Yay it works"
-    footer = json_params['footer'] || "by default"
+  image = json_params['img'] ||
+    "https://pbs.twimg.com/profile_images/378800000822867536/3f5a00acf72df93528b6bb7cd0a4fd0c.jpeg"
+  header = json_params['header'] || "Yay it works"
+  footer = json_params['footer'] || "by default"
 
-    DirectoryCleaner::clean
-    img = MemeGenerator::generate(image, header, footer)
+  DirectoryCleaner::clean
+  img = MemeGenerator::generate(image, header, footer)
 
-    return "/meme/#{img}"
-  end
+  return "/meme/#{img}"
 end
