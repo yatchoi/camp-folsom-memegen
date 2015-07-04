255ba39cd49c8a0c017d6f636f28284d0293c6ec
Yat Choi <yat.choi@airbnb.com>
2015-07-04 10:54:31 -0700

    Add config.ru

--- main.rb
+++ main.rb
@@ -3,20 +3,34 @@ require 'json'
 require "./lib/meme_generator"
 require "./lib/directory_cleaner"
 
-get '/' do
-  'Hello World!'
-end
+class MainApp < Sinatra::Base
+  get '/' do
+    'Hello World!'
+  end
 
-post '/memegen' do
-  json_params = JSON.parse(request.body.read)
+  get '/memegen' do
+    image = params['img'] ||
+      "https://pbs.twimg.com/profile_images/378800000822867536/3f5a00acf72df93528b6bb7cd0a4fd0c.jpeg"
+    header = params['header'] || "Yay it works"
+    footer = params['footer'] || "by default"
 
-  image = json_params['img'] ||
-    "https://pbs.twimg.com/profile_images/378800000822867536/3f5a00acf72df93528b6bb7cd0a4fd0c.jpeg"
-  header = json_params['header'] || "Yay it works"
-  footer = json_params['footer'] || "by default"
+    DirectoryCleaner::clean
+    img = MemeGenerator::generate(image, header, footer)
 
-  DirectoryCleaner::clean
-  img = MemeGenerator::generate(image, header, footer)
+    return "<img src='/meme/#{img}'/>"
+  end
 
-  return "/meme/#{img}"
+  post '/memegen' do
+    json_params = JSON.parse(request.body.read)
+
+    image = json_params['img'] ||
+      "https://pbs.twimg.com/profile_images/378800000822867536/3f5a00acf72df93528b6bb7cd0a4fd0c.jpeg"
+    header = json_params['header'] || "Yay it works"
+    footer = json_params['footer'] || "by default"
+
+    DirectoryCleaner::clean
+    img = MemeGenerator::generate(image, header, footer)
+
+    return "/meme/#{img}"
+  end
 end
