647839fb613e53fa7ff0d0ce553e7029bd66acd7
Yat Choi <yat.choi@airbnb.com>
2015-07-05 00:49:52 -0700

    max images

--- lib/directory_cleaner.rb
+++ lib/directory_cleaner.rb
@@ -1,8 +1,11 @@
 require 'fileutils'
 
 class DirectoryCleaner
+
+  MAX_IMAGES = 100
+
   def self.clean
-    if Dir.entries("./public/meme").size >= 7
+    if Dir.entries("./public/meme").size >= MAX_IMAGES
       delete_dir = File.join("**", "public", "**", "meme", "*jpeg")
       FileUtils.rm(Dir.glob(delete_dir))
     end
